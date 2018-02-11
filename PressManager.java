662e1f0c4df642443f94c4870cf6f20525c917ab
chsungyesuzuki <35860604+chsungyesuzuki@users.noreply.github.com>
2018-02-11 14:35:53 +0800

    Create PressManager.java
    1st version

---
+++ PressManager.java
@@ -0,0 +1,32 @@
+package chsungyesuzuki;
+import java.util.Map;
+import java.util.HashMap;
+import chsungyesuzuki.Press;
+
+public final class PressManager {
+	private static Map<Press, String> presses = new HashMap();
+	
+	private PressManager() {
+	}
+	
+	public static Press getInstanceAndCreateNew(String name, String pressNumber) {
+		Press result = new Press(name, pressNumber);
+		presses.put(result, pressNumber);
+		return result;
+	}
+	public static void remove(Press key) {
+		presses.remove(key);
+	}
+	
+	public static String getNumberInPress(Press press) {
+		return presses.get(press);
+	}
+	
+	public static ISBN getISBNInBookNumberInPress(Press press, String country, String other) {
+		String iSBNFront = "978" + country + getNumberInPress(press) + other;
+		String iSBNBack = ISBN.getBack(iSBNFront);
+		String iSBN = iSBNFront + iSBNBack;
+		ISBN result = new ISBN(iSBN);
+		return result;
+	}
+}

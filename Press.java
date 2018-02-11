e2fcb60db5dfd4133064469b0fd0a7b62f24d1b5
chsungyesuzuki <35860604+chsungyesuzuki@users.noreply.github.com>
2018-02-11 12:17:14 +0800

    Update Press.java
    2nd version

--- Press.java
+++ Press.java
@@ -3,29 +3,17 @@ import java.util.Map;
 import java.util.HashMap;
 import chsungyesuzuki.ISBN;
 public class Press {
-	private static Map<Press,String> presses = new HashMap();
+	private static Map<Press, String> presses = new HashMap();
 	private String name;
 	private String pressNumber;
-	public Press(String name,String pressNumber) {
+	public Press(String name, String pressNumber) {
 		this.name = name;
 		this.pressNumber = pressNumber;
 	}
 	public static Press getInstanceAndCreateNew(String name,String pressNumber) {
-		Press instance = new Press(name,pressNumber);
-		presses.put(instance, pressNumber);
-		return instance;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPressNumber() {
-		return pressNumber;
-	}
-	public void setPressNumber(String pressNumber) {
-		this.pressNumber = pressNumber;
+		Press result = new Press(name,pressNumber);
+		presses.put(result, pressNumber);
+		return result;
 	}
 	public static String getNumberInPress(Press press) {
 		return presses.get(press);
@@ -38,7 +26,7 @@ public class Press {
 		return result;
 	}
 	public String toString() {
-		return this.getName();
+		return getName();
 	}
 	public void remove(Press key) {
 		presses.remove(key);

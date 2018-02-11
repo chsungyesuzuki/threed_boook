2627f633da7bf40caf01b089cec40d3289e43c25
chsungyesuzuki <35860604+chsungyesuzuki@users.noreply.github.com>
2018-02-11 12:23:57 +0800

    Update Book.java
    2nd version

--- Book.java
+++ Book.java
@@ -4,66 +4,20 @@ import chsungyesuzuki.Press;
 import chsungyesuzuki.Location;
 import java.util.Calendar;
 public class Book {
-	private String name;
-	private String author;
-	private byte edition;
-	private Location location;
-	private ISBN iSBN;
-	private Press press;
-	private Calendar date;
-	private float price;
+	public String name;
+	public String author;
+	public Location location;
+	public byte edition;
+	public ISBN iSBN;
+	public Press press;
+	public Calendar date;
+	public float price;
+	public Book(){
+	}
 	public Book(String name,String author,Location location) {
 		this.name = name;
 		this.author = author;
 		this.location = location;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	public byte getEdition() {
-		return edition;
-	}
-	public void setEdition(byte edition) {
-		this.edition = edition;
-	}
-	public Location getLocation() {
-		return location;
-	}
-	public void setLocation(Location location) {
-		this.location = location;
-	}
-	public ISBN getISBN() {
-		return iSBN;
-	}
-	public void setISBN(ISBN iSBN) {
-		this.iSBN = iSBN;
-	}
-	public Press getPress() {
-		return press;
-	}
-	public void setPress(Press press) {
-		this.press = press;
-	}
-	public Calendar getDate() {
-		return date;
-	}
-	public void setDate(Calendar date) {
-		this.date = date;
-	}
-	public float getPrice() {
-		return price;
-	}
-	public void setPrice(float price) {
-		this.price = price;
 	}
 	public String toString() {
 		String result = new String();

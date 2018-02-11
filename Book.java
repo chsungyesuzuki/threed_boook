77c542ec801cf10922f87546cb04d16179005ed6
chsungyesuzuki <35860604+chsungyesuzuki@users.noreply.github.com>
2018-02-11 12:44:56 +0800

    Update Book.java
    3rd version

--- Book.java
+++ Book.java
@@ -3,6 +3,7 @@ import chsungyesuzuki.ISBN;
 import chsungyesuzuki.Press;
 import chsungyesuzuki.Location;
 import java.util.Calendar;
+
 public class Book {
 	public String name;
 	public String author;
@@ -14,11 +15,13 @@ public class Book {
 	public float price;
 	public Book(){
 	}
+	
 	public Book(String name, String author, Location location) {
 		this.name = name;
 		this.author = author;
 		this.location = location;
 	}
+	
 	public String toString() {
 		String result = new String();
 		result = "name:" + name + "author:" + author + "location:" + location.toString() + "ISBN:" + iSBN.toString();
